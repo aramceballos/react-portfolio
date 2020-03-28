a7159423e557c8b31bb6adfcba19b55bd501dc42
Aram Ceballos <aramgonzalez12@hotmail.com>
2020-03-27 21:22:16 -0700

    Certificates tab added

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,5 +4,9 @@ import { Section, Link } from './styles';
 export const Header = () => (
   <Section>
     <Link to='/'>Aram Ceballos</Link>
+    <Link style={{ float: 'right' }} to='/certificates'>
+      Certificates
+    </Link>
+    <Link to='/about'>About Me</Link>
   </Section>
 );
